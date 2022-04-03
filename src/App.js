6ae965a7e181ce63f17e2e0bf890d8d253d2d2ad
jirapong2542 <teenteen2542@gmail.com>
2022-04-04 01:48:09 +0700

    ปรับใหม่

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 
 import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import logo from './2.png';
+import Swal from 'sweetalert2'
+
 function App() {
 
 
+
+  useEffect(() => {
+
+  }, []);
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 10000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  Toast.fire({
+    icon: 'info',
+    title: 'ปรับปรุงชั่วคราว อีก 10 มาทำใหม่'
+  })
 
   return (
 
     <Container fluid style={{ color: 'white' }} className='p-5'>
 
-      <div className="cardmoon">
+      {/* <div className="cardmoon">
         <img src={logo} className="logo" />
-        {/* <div className="head">
+        <div className="head">
            <h1>NIKE AM90id</h1> 
-        </div> */}
-      </div>
-
+        </div> 
+      </div> */}
+      <iframe src='https://solarsystem.nasa.gov/gltf_embed/2366' width='100%' height='450px' frameborder='0' />
 
     </Container >
   );
